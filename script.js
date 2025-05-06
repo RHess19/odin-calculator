76477e2075ea7fc5b74f34a9788db2ff6898c66c
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-06 16:48:08 -0600

    Create boilerplate and four basic math operation functions.

---
+++ script.js
@@ -0,0 +1,22 @@
+// Basic operation functions
+function add(a, b)
+{
+    return a+b;
+}
+
+function subtract(a, b)
+{
+    return a-b;
+}
+
+function multiply(a, b)
+{
+    return a*b;
+}
+
+function divide(a, b)
+{
+    return a/b;
+}
+
+
