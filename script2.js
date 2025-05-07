82945efa4a4c8d4263a7f60f215e78c258c65b6f
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-07 15:50:29 -0600

    With script2.js - normal and chained operations work. TODO: Enable entering multi-digit numbers again

---
+++ script2.js
@@ -0,0 +1,158 @@
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+let operator2 = "";
+let result = "";
+let display = document.querySelector(".calculator-display");
+display.textContent = "";
+let chaining;
+
+// Basic operation functions
+function add(a, b)
+{
+    return (Number(a) + Number(b));
+}
+
+function subtract(a, b)
+{
+    return (Number(a) - Number(b));
+}
+
+function multiply(a, b)
+{
+    return (Number(a) * Number(b));
+}
+
+function divide(a, b)
+{
+    return (Number(a) / Number(b));
+}
+
+function operate(operator, operand1, operand2)
+{
+    switch (operator)
+    {
+        case "+":
+            return add(operand1, operand2);
+            break;
+        
+        case "-":
+            return subtract(operand1, operand2);
+            break;
+
+        case "X":
+            return multiply(operand1, operand2);
+            break;
+
+        case "/":
+            return divide(operand1, operand2);
+            break;
+    }
+}
+
+
+function addCharToDisplay(char)
+{
+    display.textContent += char;
+}
+
+function clearDisplay()
+{
+    display.textContent = "";
+}
+
+
+
+
+// Button event lister via event delegation
+const buttonsContainer = document.querySelector(".calculator-buttons-container");
+
+buttonsContainer.addEventListener("click", (event) => {
+    // Prevent from triggering on the container
+    if(event.target.className === "calculator-buttons-container")
+    {
+        return;
+    }
+
+
+    if(event.target.value === "CLEAR")
+    {
+        clearDisplay();
+        operand1 = "";
+        operand2 = "";
+        operator = "";
+    }
+    // Operator
+    else if(event.target.value === "+" || event.target.value === "-" || event.target.value === "X" || event.target.value === "/")
+    {
+        if(operand1 !== "" && operator !== "" && operand2 !== "")
+        {
+            operand1 = result;
+            operand2 = display.textContent;
+            result = operate(operator, operand1, operand2);
+            operator = event.target.value;
+            display.textContent = result;
+        } else if(operand1 !== "" && operator !== "")
+        {
+            operand2 = display.textContent;
+            result = operate(operator, operand1, operand2);
+            operator = event.target.value;
+            display.textContent = result;
+            chaining = true;
+        }
+        else
+        {
+            operand1 = display.textContent;
+            operator = event.target.value;
+            clearDisplay();
+        }
+
+        
+    }
+    else if(event.target.value === "=")
+    {
+        if(operand1 !== "" && operator !== "" && operand2 !== "")
+        {
+            operand1 = result;
+            operand2 = display.textContent;
+            result = operate(operator, operand1, operand2);
+            display.textContent = result;
+            operand1 = "";
+            operand2 = "";
+            operator = "";
+            operator2 = "";
+            chaining = "";
+            result = "";
+            return;
+        }
+
+        operand2 = display.textContent;
+        result = operate(operator, operand1, operand2);
+        display.textContent = result;
+
+        // Reset all calculation values for the next calculation
+        operand1 = "";
+        operand2 = "";
+        operator = "";
+        operator2 = "";
+        chaining = "";
+        result = "";
+    }
+    // Number or decimal
+    else
+    {
+        if(result === "")
+        {
+            clearDisplay();
+        }
+        if(chaining == true)
+        {
+            clearDisplay();
+            addCharToDisplay(event.target.value);
+        }
+        else
+        {
+            addCharToDisplay(event.target.value);
+        }
+    }
+});
